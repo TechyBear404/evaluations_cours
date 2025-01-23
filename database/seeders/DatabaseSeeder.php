08436a2c0d103037b4d978b7fba96ea07be37b4a
BlackPigCompany <86426768+BlackPigCompany@users.noreply.github.com>
2025-01-23 14:06:20 +0100

    feat(models-seeder): ajouter le modèle Component avec sa factory et mise à jour du seeder pour inclure des composants et des cours prédéfinis

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Component;
 use App\Models\Course;
 use App\Models\Inscription;
 use App\Models\Student;
@@ -23,10 +24,42 @@ class DatabaseSeeder extends Seeder
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
-
         Student::factory(10)->create();
         Teacher::factory(10)->create();
-        Course::factory(5)->create();
+
+        $courses = [
+            "PHP",
+            "JavaScript",
+            "Design",
+            "Projet web Dynamique",
+            "Projet web Statique",
+        ];
+        foreach ($courses as $course) {
+            Course::factory()->create([
+                'name' => $course,
+            ]);
+        }
         Inscription::factory(10)->create();
+
+        Component::create([
+            'name' => 'champs de texte court',
+            'type' => 'input',
+        ]);
+        Component::create([
+            'name' => 'champs de texte long',
+            'type' => 'textarea',
+        ]);
+        Component::create([
+            'name' => 'choix unique',
+            'type' => 'radio',
+        ]);
+        Component::create([
+            'name' => 'choix multiple',
+            'type' => 'checkbox',
+        ]);
+        Component::create([
+            'name' => 'tableau de choix',
+            'type' => 'table_radio',
+        ]);
     }
 }
