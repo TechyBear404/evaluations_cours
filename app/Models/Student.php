71322118a5f42475183f73a1adaf6b7b55f46b9e
BlackPigCompany <86426768+BlackPigCompany@users.noreply.github.com>
2025-01-28 11:20:42 +0100

    feat(ui): ajouter la gestion des formulaires avec affichage, envoi et notifications

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -4,10 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
 
 class Student extends Model
 {
     use HasFactory;
+    use Notifiable;
     protected $fillable = ['email'];
 
     public function courses()
