bc9e06b49f2c28fac871278b61c6bd79a3a460ba
BlackPigCompany <86426768+BlackPigCompany@users.noreply.github.com>
2025-02-01 10:15:35 +0100

    feat(navigation): mettre à jour les liens de navigation et ajouter des icônes

--- routes/web.php
+++ routes/web.php
@@ -12,12 +12,14 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
+    // return Inertia::render('Welcome', [
+    //     'canLogin' => Route::has('login'),
+    //     'canRegister' => Route::has('register'),
+    //     'laravelVersion' => Application::VERSION,
+    //     'phpVersion' => PHP_VERSION,
+    // ]);
+
+    return redirect()->route('courses.Index');
 });
 
 Route::middleware([
@@ -25,9 +27,9 @@ Route::middleware([
     config('jetstream.auth_session'),
     'verified',
 ])->group(function () {
-    Route::get('/dashboard', function () {
-        return Inertia::render('Dashboard');
-    })->name('dashboard');
+    // Route::get('/dashboard', function () {
+    //     return Inertia::render('Dashboard');
+    // })->name('dashboard');
 
     Route::get('/form', [FormController::class, 'index'])->name('form.index');
     Route::get('/form/create', [FormController::class, 'create'])->name('form.create');
